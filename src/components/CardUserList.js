04837b195d8c170f1710691a4d04cf1f320f0131
Tobias Maymo <tobiasmmaymo@gmail.com>
2022-09-06 23:42:45 -0300

    second commit

--- src/components/CardUserList.js
+++ src/components/CardUserList.js
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import CardUser from './CardUser';
 import axios from 'axios';
+import Spinner from './Spinner/Spinner';
 
 const CardUserList = () => {
 	const [users, setUsers] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
 	useEffect(() => {
+		setIsLoading(true);
 		axios('https://dragon-ball-super-api.herokuapp.com/api/characters').then((res) =>
-			setUsers(res.data)
-		);
+			setUsers(res.data));
+
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 1000);
 	}, []);
 
 	return (
@@ -16,7 +22,7 @@ const CardUserList = () => {
 			{users.map((user) => {
 				return (
 					<div key={user.id}>
-							<CardUser data={user} />
+							{isLoading ? <Spinner /> : <CardUser data={user} />}
 					</div>
 				);
 			})}
